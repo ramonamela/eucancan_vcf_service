6917c10ce8c47d478e7a187d394edac9140e0ac6
ramonamela <ramon.amela@gmail.com>
2019-10-04 12:00:17 +0200

    PUT by POST in local test

--- local_test.sh
+++ local_test.sh
@@ -2,9 +2,9 @@
 
 target_ip="84.88.186.194"
 
-curl -X POST -F "vcf_file=@$(pwd)/test_files/test.txt" "http://${target_ip}:5000/files/"
-curl -X POST -F "vcf_file=@$(pwd)/test_files/test.vcf.gz" "http://${target_ip}:5000/files/"
-curl -X POST -F "vcf_file=@$(pwd)/test_files/test.vcf.gz" "http://${target_ip}:5000/files/"
+curl -X PUT -F "vcf_file=@$(pwd)/test_files/test.txt" "http://${target_ip}:5000/files/"
+curl -X PUT -F "vcf_file=@$(pwd)/test_files/test.vcf.gz" "http://${target_ip}:5000/files/"
+curl -X PUT -F "vcf_file=@$(pwd)/test_files/test.vcf.gz" "http://${target_ip}:5000/files/"
 curl -X GET "http://${target_ip}:5000/files/test.vcf.gz"
 curl -X DELETE "http://${target_ip}:5000/files/test.vcf.gz"
 curl -X DELETE "http://${target_ip}:5000/files/test.vcf.gz"
